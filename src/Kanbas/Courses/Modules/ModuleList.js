416588669f152faeb127ee697c6db0162cb03434
tarifkhan <tarif.ny@gmail.com>
2023-10-31 14:34:08 -0400

    Modules

--- src/Kanbas/Courses/Modules/ModuleList.js
+++ src/Kanbas/Courses/Modules/ModuleList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import db from "../../Database";
 import "./index.css";
@@ -10,77 +10,138 @@ import {
   FaGripVertical,
 } from "react-icons/fa";
 
+import { useSelector, useDispatch } from "react-redux";
+import {
+  addModule,
+  deleteModule,
+  updateModule,
+  setModule,
+} from "./modulesReducer";
+
 function ModuleList() {
   const { courseId } = useParams();
-  const modules = db.modules;
+  const modules = useSelector((state) => state.modulesReducer.modules);
+  const module = useSelector((state) => state.modulesReducer.module);
+  const dispatch = useDispatch();
+
   return (
-
-
-<div className="Content">
-    <div class="buttonRow">
+    <div className="Content">
+      <div class="buttonRow">
         <span id="threeDots" class="btn btn-outline-secondary">
-        <FaEllipsisV /></span>
+          <FaEllipsisV />
+        </span>
 
         <span id="EditButton" class="btn btn-danger">
-        <FaPlus />Module</span>
+          <FaPlus />
+          Module
+        </span>
 
         <span id="EditButton" class="btn btn-outline-secondary">
-        <FaCheckCircle color="green" />Publish All</span>
+          <FaCheckCircle color="green" />
+          Publish All
+        </span>
 
-        <span id="EditButton" class="btn btn-outline-secondary">View Progress</span>
+        <span id="EditButton" class="btn btn-outline-secondary">
+          View Progress
+        </span>
 
-        <span id="EditButton" class="btn btn-outline-secondary">Collapse All</span>
+        <span id="EditButton" class="btn btn-outline-secondary">
+          Collapse All
+        </span>
+      </div>
+      <hr />
 
-       
-    </div>
-    <hr />
-                    
-    <ul className="list-group">
-      {modules
-        .filter((module) => module.course === courseId)
-        .map((module, index) => (
-          <div className="HomeWeek" key={index}>
-            <div className="HomeWeek">
-              <div class="list-group-item list-group-item-secondary py-3">
-                <FaGripVertical />
-                <FaCaretDown />
-                <span Class="Week">{module.week}</span>
-                <div class="float-end">
-                  <FaCheckCircle color="green" />
+      <ul className="list-group">
+        <li className="list-group-item">
+          <button
+            className="btn btn-success"
+            onClick={() => dispatch(addModule({ ...module, course: courseId }))}
+          >
+            Add
+          </button>
+
+          <button
+            className="btn btn-primary"
+            onClick={() => dispatch(updateModule(module))}
+          >
+            Update
+          </button>
+
+          <input
+            className="form-control"
+            value={module.name}
+            onChange={(e) =>
+              dispatch(setModule({ ...module, name: e.target.value }))
+            }
+          />
+          <textarea
+            className="form-control"
+            value={module.description}
+            onChange={(e) =>
+              dispatch(setModule({ ...module, description: e.target.value }))
+            }
+          />
+        </li>
+
+        {modules
+          .filter((module) => module.course === courseId)
+          .map((module, index) => (
+            <div className="HomeWeek" key={index}>
+              <div className="HomeWeek">
+                <div class="list-group-item list-group-item-secondary py-3">
+                  <FaGripVertical />
                   <FaCaretDown />
-                  <FaPlus />
-                  <FaEllipsisV />
-                </div>
-              </div>
-            </div>
+                  <span Class="Week">{module.week}</span>
 
-            <div class="HomeItems">
-              <div class="list-group-item py-3">
-                <FaGripVertical />
-                <span Class="Dimension1">
-                  LEARNING OBJECTIVES - {module.name}
-                </span>
-                <div class="float-end">
-                  <FaCheckCircle color="green" />
-                  <FaEllipsisV />
-                </div>
-              </div>
-            </div>
+                  <div class="float-end">
+                    <button
+                      className="btn btn-danger"
+                      onClick={() => dispatch(deleteModule(module._id))}
+                    >
+                      Delete
+                    </button>
+                    <button
+                      className="btn btn-success"
+                      onClick={() => dispatch(setModule(module))}
+                    >
+                      Edit
+                    </button>
 
-            <div Class="HomeItemOItem">
-              <div class="list-group-item py-3">
-                <FaGripVertical />
-                <span Class="Dimension1">{module.description}</span>
-                <div class="float-end">
-                  <FaCheckCircle color="green" />
-                  <FaEllipsisV />
+                    <FaCheckCircle color="green" />
+                    <FaCaretDown />
+                    <FaPlus />
+                    <FaEllipsisV />
+                  </div>
+                </div>
+              </div>
+
+              <div class="HomeItems">
+                <div class="list-group-item py-3">
+                  <FaGripVertical />
+                  <span Class="Dimension1">
+                    LEARNING OBJECTIVES - {module.name}
+                  </span>
+                  <div class="float-end">
+                    <FaCheckCircle color="green" />
+                    <FaEllipsisV />
+                  </div>
+                </div>
+              </div>
+
+              <div Class="HomeItemOItem">
+                <div class="list-group-item py-3">
+                  <FaGripVertical />
+                  <span Class="Dimension1">{module.description}</span>
+                  <div class="float-end">
+                    <FaCheckCircle color="green" />
+                    <FaEllipsisV />
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
-    </ul>
-</div>
+          ))}
+      </ul>
+    </div>
   );
 }
 export default ModuleList;
