dcd614bb747c1645e3a223e11d667bec2c83ab8c
tarifkhan <tarif.ny@gmail.com>
2023-10-31 02:08:25 -0400

    Lab Completed A4

--- src/Labs/a3/index.js
+++ src/Labs/a3/index.js
@@ -2,16 +2,26 @@ import JavaScript from "./JavaScript";
 import Styles from "./Styles";
 import ConditionalOutput from "./ConditionalOutput";
 import TodoList from "./todo/TodoList";
+import { useSelector } from "react-redux";
 
 function Assignment3() {
-    return (
-      <div>
-        <h1>Assignment 3</h1>
-        <JavaScript/>
-        <Styles />
-        <ConditionalOutput />
-        <TodoList />
-      </div>
-    );
-  }
-  export default Assignment3;
+  const { todos } = useSelector((state) => state.todosReducer);
+  return (
+    <div>
+      <h1>Assignment 3</h1>
+      <JavaScript />
+      <Styles />
+      <ConditionalOutput />
+      <TodoList />
+
+      <ul className="list-group">
+        {todos.map((todo) => (
+          <li className="list-group-item" key={todo.id}>
+            {todo.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+export default Assignment3;
