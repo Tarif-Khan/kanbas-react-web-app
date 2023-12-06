b6d57c663d7c26e7cd2b3a5fff75a034e339792a
tarifkhan <tarif.ny@gmail.com>
2023-12-06 14:14:26 -0500

    Update client.js

--- src/Kanbas/Courses/Modules/client.js
+++ src/Kanbas/Courses/Modules/client.js
@@ -1,11 +1,14 @@
 import axios from "axios";
-const COURSES_URL = "http://localhost:4000/api/courses";
+
 // const MODULES_URL = "http://localhost:4000/api/modules";
 // const MODULES_URL =
 //   "https://kanbas-node-server-app-cs1234-sp23.onrender.com/api/modules";
 // Testing
 const API_BASE = process.env.REACT_APP_API_BASE;
 const MODULES_URL = `${API_BASE}/api/modules`;
+const COURSES_URL = `${API_BASE}/api/courses`;
+console.log(MODULES_URL);
+console.log(COURSES_URL);
 
 export const createModule = async (courseId, module) => {
   const response = await axios.post(
