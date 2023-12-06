d7b98fe02bd0f56aecbeb8c755c5b27160723243
tarifkhan <tarif.ny@gmail.com>
2023-12-06 14:11:31 -0500

    Update index.js

--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -21,7 +21,7 @@ function Kanbas() {
 
   // const URL = "http://localhost:4000/api/courses";
   const API_BASE = process.env.REACT_APP_API_BASE;
-  const URL = `${API_BASE}/courses`;
+  const URL = `${API_BASE}/api/courses`;
   console.log(URL);
 
   const findAllCourses = async () => {
